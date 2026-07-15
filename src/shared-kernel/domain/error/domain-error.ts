dc2363cc2a6a6737ae2871a957225330513701a2
DFedizko <fedizkopedro@gmail.com>
2026-07-15 09:53:27 -0300

    chore(domain-error): optional status property removed

--- src/shared-kernel/domain/error/domain-error.ts
+++ src/shared-kernel/domain/error/domain-error.ts
@@ -3,9 +3,8 @@ import { BaseError, type BaseErrorProps } from './base-error'
 export class DomainError extends BaseError {
   constructor({
     message = 'Domain error',
-    status = 422,
     code = 'DOMAIN_ERROR',
   }: BaseErrorProps) {
-    super({ message, status, code })
+    super({ message, status: 422, code })
   }
 }
